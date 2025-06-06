983827f5ebe608dfaf15042ea84599213f311119
Your Name <a.mura27400@gmail.com>
2025-06-06 23:34:06 +0200

    refactor: remove unused useParties hook

--- hooks/useParties.ts
+++
@@ -1,8 +0,0 @@
-import { useApi } from '@/hooks/useApi';
-import { PartyType } from '@/types/PartyType';
-
-export function useParties() {
-  const [parties, isLoading, error] = useApi<PartyType[]>('/parties');
-
-  return { parties, isLoading, error };
-}
